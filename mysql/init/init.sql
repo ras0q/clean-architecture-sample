3b777634eeb803d4f71ccfb5a79f58c3e80cf86e
Ras96 <asymptote.k.k@gmail.com>
2021-09-10 11:53:48 +0900

    ✨ sample data
    gp

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS clean-architecture-sample;
-CREATE DATABASE clean-architecture-sample;
-USE clean-architecture-sample;
+DROP DATABASE IF EXISTS `clean-architecture-sample`;
+CREATE DATABASE `clean-architecture-sample`;
+USE `clean-architecture-sample`;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` char(36) PRIMARY KEY NOT NULL,
@@ -8,3 +8,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(32) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- sample data
+INSERT INTO `users` (`id`, `name`, `email`) VALUES (UUID(), 'John Doe', 'jonny@example.com');
+INSERT INTO `users` (`id`, `name`, `email`) VALUES (UUID(), 'Mike Smith', 'mike@example.com');
